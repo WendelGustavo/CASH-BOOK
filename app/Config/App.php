9f962fea2ba120005a6e7e11a594f165852346c6
Wendel Gustavo <wendelfi66@gmail.com>
2022-11-14 16:26:17 -0300

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/cashbook/';
+    public $baseURL = 'cashbookrelatorios.herokuapp.com';
 
     /**
      * --------------------------------------------------------------------------
